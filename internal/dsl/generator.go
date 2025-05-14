7760981ed8ec1e174fc9c534c18929d82fb1ae61
Martin Bozhilov <marogo142005@gmail.com>
2025-05-14 20:55:28 +0200

    Release v0.2.6-beta
    UUID & time lib

--- internal/dsl/generator.go
+++ internal/dsl/generator.go
@@ -14,15 +14,42 @@ type Generator struct {
 }
 
 func NewGenerator() *Generator {
+	funcMap := template.FuncMap{
+		"lower":     strings.ToLower,
+		"hasTime":   hasTime,
+		"hasUUID":   hasUUID,
+		"buildTags": buildTags,
+	}
 	tmpl := template.Must(template.
 		New("entity").
-		Funcs(template.FuncMap{
-			"lower": func(s string) string {
-				return strings.ToLower(s)
-			},
-		}).
+		Funcs(funcMap).
 		Parse(entityTemplate))
 	return &Generator{Template: tmpl}
+}
+
+// hasTime returns true if any field uses time.Time.
+func hasTime(fields []Field) bool {
+	for _, f := range fields {
+		if f.Type == "time.Time" {
+			return true
+		}
+	}
+	return false
+}
+
+// hasUUID returns true if any field uses uuid.UUID.
+func hasUUID(fields []Field) bool {
+	for _, f := range fields {
+		if f.Type == "uuid.UUID" {
+			return true
+		}
+	}
+	return false
+}
+
+// buildTags is a placeholder for tag building logic.
+func buildTags(f Field) string {
+	return "  `db:\"" + strings.ToLower(f.Name) + "\"`"
 }
 
 func (g *Generator) Generate(ast AST, outDir string) error {
@@ -41,9 +68,20 @@ func (g *Generator) Generate(ast AST, outDir string) error {
 	return nil
 }
 
-const entityTemplate = `package {{.Name}}
+const entityTemplate = `package {{ lower .Name }}
+
+import (
+{{- if hasTime .Fields }}
+    "time"
+{{- end }}
+{{- if hasUUID .Fields }}
+    uuid "github.com/google/uuid"
+{{- end }}
+)
+
 type {{ .Name }} struct {
 {{- range .Fields }}
-{{.Name}} {{.Type}}  ` + "`" + `db:"{{.Name | lower}}"` + "`" + `
+    {{ .Name }} {{ .Type }}{{ buildTags . }}
 {{- end }}
-}`
+}
+`
