2fa10688a2c4e05363fc8edd73c742192d8b9e82
Martin Bozhilov <marogo142005@gmail.com>
2025-05-14 15:17:21 +0200

    [ADD] torm main func

---
+++ pkg/torm/torm.go
@@ -0,0 +1,33 @@
+// File: pkg/torm/torm.go
+package torm
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/TechXTT/TORM/internal/core"
+)
+
+// DB is the main handle for executing queries
+type DB struct {
+	conn *sql.DB
+}
+
+// Open connects to the database and returns a DB
+func Open(dsn string) (*DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{conn: db}, nil
+}
+
+// Query returns a new query builder
+func Query[T any](d *DB) *core.QueryBuilder[T] {
+	return core.NewQueryBuilder[T](d.conn)
+}
+
+// Exec executes raw SQL with context
+func (d *DB) Exec(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	return d.conn.ExecContext(ctx, query, args...)
+}
